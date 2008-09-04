749f22f4857072a1fbe067d4cf653c7f54d77373
Mark Yoon <yoon@northwestern.edu>
2008-09-04 15:21:47 -0500

    update to readme, description

--- mail_to_extension.rb
+++ mail_to_extension.rb
@@ -1,7 +1,7 @@
 class MailToExtension < Radiant::Extension
   version "0.1"
   description "MailTo is a Radiant CMS extension that hides e-mails from robots. It is a simple wrapper for the Rails mail_to tag with :encode => :javascript."
-  url "http://yourwebsite.com/mail_to"
+  url "http://github.com/yoon/radiant-mail-to-extension"
   
   def activate
     Page.send :include, MailToTag
